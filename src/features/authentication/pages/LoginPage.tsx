c0f4e236b934854e1068537562e8237427efb76d
ModitTdr <modittuladhar@gmail.com>
2026-05-12 11:21:45 +0545

    feature: login/register page with validation

--- src/features/authentication/pages/LoginPage.tsx
+++ src/features/authentication/pages/LoginPage.tsx
@@ -1,6 +1,53 @@
+import Button from "@/components/atom/Button"
+import { useForm } from "react-hook-form"
+import LoginSchema from "../schema/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import InputField from "@/components/molecule/InputField";
+import { Link } from "react-router";
+
 const LoginPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  });
+
+  const onSubmit = (data: { email: string, password: string }) => {
+    console.log(data);
+  }
+
   return (
-    <div>LoginPage</div>
+    <section className="space-y-16 w-full max-w-[440px] p-10 py-14 shadow-md border border-foreground-muted/10 rounded-sm">
+      <div className="text-center space-y-1">
+        <h1 className="text-4xl lg:text-[3vw] font-bold leading-none">Sign In</h1>
+        <p className="text-lg lg:text-[1.2vw] text-foreground-muted">Login to your account to get started</p>
+      </div>
+      <form className="text-center space-y-8" onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-5">
+          <InputField
+            label="Email"
+            type="email"
+            {...register("email", { required: "Email required" })}
+            error={errors.email?.message}
+          />
+          <InputField
+            label="Password"
+            type="password"
+            {...register("password", { required: "Password required" })}
+            error={errors.password?.message}
+          />
+        </div>
+        <Button>
+          Sign In
+        </Button>
+
+        <div className="text-foreground-muted">
+          <p>Don't have an account? <Link to="/register" className="text-foreground">Register</Link></p>
+        </div>
+      </form>
+    </section>
   )
 }
 
--- src/features/authentication/pages/RegisterPage.tsx
+++ src/features/authentication/pages/RegisterPage.tsx
@@ -1,7 +1,61 @@
+import Button from "@/components/atom/Button"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import InputField from "@/components/molecule/InputField";
+import { Link } from "react-router";
+import RegisterSchema from "../schema/registerSchema";
 
 const RegisterPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }
+  });
+
+  const onSubmit = (data: { email: string, password: string, confirmPassword: string }) => {
+    console.log(data);
+  }
+
   return (
-    <div>RegisterPage</div>
+    <section className="space-y-16 w-full max-w-[440px] p-10 py-14 shadow-md border border-foreground-muted/10 rounded-sm">
+      <div className="text-center space-y-1">
+        <h1 className="text-4xl lg:text-[3vw] font-bold leading-none">Register</h1>
+        <p className="text-lg lg:text-[1.2vw] text-foreground-muted">Create an account to get started</p>
+      </div>
+      <form className="text-center space-y-8" onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-5">
+          <InputField
+            label="Email"
+            type="email"
+            {...register("email", { required: "Email required" })}
+            error={errors.email?.message}
+          />
+          <InputField
+            label="Password"
+            type="password"
+            showForPassword={false}
+            {...register("password", { required: "Password required" })}
+            error={errors.password?.message}
+          />
+          <InputField
+            label="Confirm Password"
+            type="password"
+            {...register("confirmPassword", { required: "Confirm Password required" })}
+            error={errors.confirmPassword?.message}
+          />
+        </div>
+        <Button>
+          Sign In
+        </Button>
+
+        <div className="text-foreground-muted">
+          <p>Already have a account? <Link to="/login" className="text-foreground">Login</Link></p>
+        </div>
+      </form>
+    </section>
   )
 }
 
