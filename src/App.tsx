f78d76574824b39822ec5d641b0fab80dd999227
ModitTdr <modittuladhar@gmail.com>
2026-05-13 14:20:41 +0545

    feature: context to check user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router";
 import router from "./routes/router";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
       <Toaster />
+      <AuthProvider>
           <RouterProvider router={router} />
+      </AuthProvider>
     </>
   )
 }
