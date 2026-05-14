ab1896850816b8e7a67ae9d5945dc20d9a342c15
ModitTdr <modittuladhar@gmail.com>
2026-05-14 16:51:57 +0545

    feature:currency selector

--- src/components/organism/Navbar.tsx
+++ src/components/organism/Navbar.tsx
@@ -2,8 +2,12 @@ import { Activity, HomeIcon, LayoutDashboard, LogOut, Settings } from "lucide-re
 import { Link, NavLink, useNavigate } from "react-router"
 import { logOut } from "@/features/authentication/services/authService";
 import Button from "../atom/Button";
+import Select from "../atom/Select";
+import { useCurrency, type Currency } from "@/context/CurrencyContext";
 
 const Navbar = () => {
+  const { currency, changeCurrency } = useCurrency();
+
   const navigate = useNavigate();
   const navLinks = [
     { link: '/dashboard', label: 'Home', icon: HomeIcon },
@@ -19,20 +23,20 @@ const Navbar = () => {
   return (
     <header className="w-full sticky top-0 z-50 bg-background/80 backdrop-blur-sm">
       <nav className="h-15 flex items-center justify-between px-10 border-b border-black/5">
-        <Link to="/" className="font-bold tracking-[0.2em] uppercase flex items-center gap-2 font-sora">
+        <Link to="/" className="font-bold tracking-[0.2em] uppercase flex items-center gap-2 font-sora hover:text-primary transition-all duration-150">
           <Activity size={18} />
           Crypto<span className="opacity-30 -ml-2 font-sora">Pulse</span>
         </Link>
 
-        <div className="flex items-center gap-10">
-          <ul className="flex items-center gap-10">
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center justify-end gap-8">
             {navLinks.map((item) => (
               <li key={item.link}>
                 <NavLink
                   to={item.link}
                   className={
                     ({ isActive }) =>
-                      `text-xs font-bold uppercase tracking-[0.2em] transition-opacity duration-300
+                      `text-sm font-bold tracking-wide transition-opacity duration-300
                        ${isActive ? 'opacity-100 text-primary' : 'opacity-30 hover:opacity-100'}`
                   }
                 >
@@ -42,15 +46,27 @@ const Navbar = () => {
             ))}
           </ul>
 
-          <Button
-            variant="ghost"
-            size="sm"
-            onClick={handleLogout}
-            className="w-auto p-0 opacity-30 hover:opacity-100 text-xs font-bold uppercase tracking-[0.2em] flex items-center gap-2"
-          >
-            <LogOut size={14} />
-            Logout
-          </Button>
+          <div className="flex items-center gap-8 justify-between w-full">
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={handleLogout}
+              className="w-auto p-0 opacity-30 hover:opacity-100 text-sm font-bold tracking-wide flex items-center gap-2 pt-1"
+            >
+              <LogOut size={14} />
+              Logout
+            </Button>
+
+            <Select
+              options={[
+                { value: 'usd', label: 'USD' },
+                { value: 'eur', label: 'EUR' },
+                { value: 'btc', label: 'BTC' },
+              ]}
+              onChange={(e) => changeCurrency(e.target.value as Currency)}
+              value={currency}
+            />
+          </div>
         </div>
       </nav>
     </header>
