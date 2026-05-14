61fdc174cc38794614936e43c113cad4821d6666
ModitTdr <modittuladhar@gmail.com>
2026-05-14 15:54:09 +0545

    refactor: colors-added, axios location changed, moved provider to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { RouterProvider } from "react-router";
 import router from "./routes/router";
 import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
       <Toaster />
-      <AuthProvider>
-          <RouterProvider router={router} />
-      </AuthProvider>
+      <RouterProvider router={router} />
     </>
   )
 }
