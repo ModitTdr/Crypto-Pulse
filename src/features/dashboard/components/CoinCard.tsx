b56995de335c61a9b0920340e1b3dfaaedf2ac93
ModitTdr <modittuladhar@gmail.com>
2026-05-17 12:42:19 +0545

    optimization: prventing card re-render on each data fetch

--- src/features/dashboard/components/CoinCard.tsx
+++ src/features/dashboard/components/CoinCard.tsx
@@ -2,6 +2,7 @@ import type { CoinResponseType } from '../types/coinResponseType';
 import { getCurrencySymbol } from '@/utils/getCurrencySymbol';
 import Badge from '@/components/atom/Badge';
 import { Card } from '@/components/atom/Card';
+import { memo } from 'react';
 
 interface CoinCardProps {
   data: CoinResponseType;
@@ -9,6 +10,7 @@ interface CoinCardProps {
   currencyType: string;
 }
 const CoinCard = ({ data, index, currencyType }: CoinCardProps) => {
+  console.log('coin card rendered', data.name)
   return (
     <Card className=' w-[440px] h-[240px]'>
       <div className="absolute -right-10 -bottom-10 w-60 opacity-10 grayscale group-hover:grayscale-0 group-hover:opacity-50 transition-all duration-700 rotate-12 group-hover:rotate-0">
@@ -44,4 +46,4 @@ const CoinCard = ({ data, index, currencyType }: CoinCardProps) => {
   )
 }
 
-export default CoinCard
+export default memo(CoinCard)
